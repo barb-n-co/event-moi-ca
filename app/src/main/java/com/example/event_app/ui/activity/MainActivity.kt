60891f424c6b489f10c49a9e6aa3a8796399f7f8
Carlos <carlos.chastagnier@gmail.com>
2019-04-30 16:42:44 +0200

    design app

--- app/src/main/java/com/example/event_app/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/event_app/ui/activity/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.event_app.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

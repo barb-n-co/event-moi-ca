6c6c9096934a2a41a156a46e89b460e0b5df67f7
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-05-03 10:17:23 +0200

    feat/ added bottomSheet and push picture to FireStore from galery and camera
    Took 3 hours 26 minutes

--- app/src/main/java/com/example/event_app/ui/fragment/DetailEventFragment.kt
+++ app/src/main/java/com/example/event_app/ui/fragment/DetailEventFragment.kt
@@ -1,10 +1,17 @@
 package com.example.event_app.ui.fragment
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.PermissionChecker
 import androidx.core.view.ViewCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -13,6 +20,7 @@ import com.example.event_app.adapter.CustomAdapter
 import com.example.event_app.model.Event
 import com.example.event_app.model.Photo
 import com.example.event_app.viewmodel.DetailEventViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import io.reactivex.rxkotlin.addTo
 import kotlinx.android.synthetic.main.fragment_detail_event.*
 import org.json.JSONObject
@@ -27,6 +35,12 @@ class DetailEventFragment : BaseFragment() {
     private var json4 = JSONObject()
     private var json5 = JSONObject()
     private val viewModel : DetailEventViewModel by instance(arg = this)
+    private val adapter = CustomAdapter()
+    private val PERMISSION_ALL = 1
+    private val PERMISSION_IMPORT = 2
+    private val IMAGE_PICK_CODE = 1000
+    private val CAPTURE_PHOTO = 104
+    private var imagePath: String? = ""
 
 
     var imageIdList = ArrayList<Photo>()
@@ -57,7 +71,10 @@ class DetailEventFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var mockEvent = Event(42, "MockEvent", loremIpsum, "Maintenant", "Jamais !!")
+        val mockEvent = Event(42, "MockEvent", loremIpsum, "Maintenant", "Jamais !!")
+
+        val nestedScrollView = view.findViewById(R.id.bottomSheetScrollView) as View
+        val sheetBehavior = BottomSheetBehavior.from(nestedScrollView)
 
         tv_eventOrga.text = "Tritri"
         tv_eventName.text = mockEvent.name
@@ -65,15 +82,47 @@ class DetailEventFragment : BaseFragment() {
         tv_eventDateEnd.text = mockEvent.dateEnd
         tv_eventDescription.text = mockEvent.description
 
+        btn_camera.setOnClickListener {
+            val permissions = arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
 
-        viewModel.fetchImagesFromFolder("jpg.png").subscribe(
-                {
-                    uri ->
-                    imageIdList.add(Photo(JSONObject().put("url", uri.toString())))
-                },
-                {
-                        throwable -> Log.e("RxFirebaseSample", throwable.toString())
-                }).addTo(viewDisposable)
+            if (PermissionChecker.checkSelfPermission(
+                    context!!,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions( permissions, PERMISSION_ALL)
+            } else {
+                takePhotoByCamera()
+            }
+        }
+
+        btn_gallery.setOnClickListener {
+            val permissions = arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+
+            if (PermissionChecker.checkSelfPermission(
+                    context!!,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(permissions, PERMISSION_IMPORT)
+            } else {
+                startActivityForResult(viewModel.pickImageFromGallery(), IMAGE_PICK_CODE)
+            }
+        }
+
+        fab_add_photo.setOnClickListener {
+            if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+            } else {
+                sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED)
+            }
+        }
 
         imageIdList.add(Photo(json))
         imageIdList.add(Photo(json2))
@@ -81,11 +130,8 @@ class DetailEventFragment : BaseFragment() {
         imageIdList.add(Photo(json4))
         imageIdList.add(Photo(json5))
 
-        val image = context!!.getDrawable(R.drawable.pic1)
 
-        viewModel.putImage(image)
 
-        val adapter = CustomAdapter()
         //val mGrid = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.HORIZONTAL)
         val mGrid = GridLayoutManager(context, 2)
         rv_listImage.layoutManager = mGrid
@@ -105,6 +151,64 @@ class DetailEventFragment : BaseFragment() {
 
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.fetchImagesFromFolder("images/image2.png").subscribe(
+            {
+                    uri ->
+                imageIdList.add(Photo(JSONObject().put("url", uri.toString())))
+                adapter.submitList(imageIdList)
+                adapter.notifyDataSetChanged()
+            },
+            {
+                    throwable -> Log.e("RxFirebaseSample", throwable.toString())
+            })
+            .addTo(viewDisposable)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_ALL && grantResults.size == 2) {
+            takePhotoByCamera()
+        }
+
+        if (requestCode == PERMISSION_IMPORT && grantResults.size == 2) {
+            startActivityForResult(viewModel.pickImageFromGallery(), IMAGE_PICK_CODE)
+        }
+    }
+
+    private fun takePhotoByCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAPTURE_PHOTO)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, returnIntent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, returnIntent)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+
+                CAPTURE_PHOTO -> {
+                    val capturedBitmap = returnIntent?.extras!!.get("data") as Bitmap
+                    //viewModel.saveImage(capturedBitmap)
+                    viewModel.putImageWithBitmap(capturedBitmap)
+                }
+
+                IMAGE_PICK_CODE ->{
+                    returnIntent?.extras
+                    val galleryUri = returnIntent?.data!!
+                    val galeryBitmap = viewModel.getBitmapWithResolver(context!!.contentResolver, galleryUri)
+                    viewModel.putImageWithBitmap(galeryBitmap)
+                }
+
+                else -> {
+                }
+            }
+
+        }
+
+    }
 
 
 }
--- app/src/main/java/com/example/event_app/viewmodel/DetailEventViewModel.kt
+++ app/src/main/java/com/example/event_app/viewmodel/DetailEventViewModel.kt
@@ -1,35 +1,43 @@
 package com.example.event_app.viewmodel
 
+import android.content.ContentResolver
+import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.event_app.repository.EventRepository
 import durdinapps.rxfirebase2.RxFirebaseStorage
-import io.reactivex.Flowable
+import io.reactivex.Observable
 import timber.log.Timber
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
 
 
 class DetailEventViewModel(private val eventRepository: EventRepository) : BaseViewModel() {
 
     private val url = "https://firebasestorage.googleapis.com/v0/b/event-moi-ca.appspot.com/o/"
 
-    fun fetchImagesFromFolder(url: String): Flowable<Uri> {
-        return RxFirebaseStorage.getDownloadUrl(eventRepository.db.getReferenceFromUrl(fullURL(url))).toFlowable()
+    fun fetchImagesFromFolder(url: String): Observable<Uri> {
+        return RxFirebaseStorage.getDownloadUrl(eventRepository.db.getReferenceFromUrl(fullURL(url))).toObservable()
     }
 
-    fun putImage(drawable: Drawable) {
+    fun putImageWithBitmap(bitmap: Bitmap) {
 
-        val bitmap = (drawable as BitmapDrawable).bitmap
         val baos = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos)
         val data = baos.toByteArray()
 
-        RxFirebaseStorage.putBytes(eventRepository.ref.child("images/image.png"),data).toFlowable().subscribe(
+        val generator = Random()
+        var n = 10000
+        n = generator.nextInt(n)
+
+        RxFirebaseStorage.putBytes(eventRepository.ref.child("images/image$n.png"),data).toFlowable().subscribe(
             {
                 Log.d("youpee", it.uploadSessionUri.toString())
             },
@@ -38,11 +46,45 @@ class DetailEventViewModel(private val eventRepository: EventRepository) : BaseV
             }
         )
 
-
     }
+
+    fun getBitmapWithResolver(resolver: ContentResolver, uri: Uri): Bitmap {
+        return MediaStore.Images.Media.getBitmap(resolver, uri)
+    }
+
 
     private fun fullURL(foldrUrl: String): String {
         return url + foldrUrl
+    }
+
+    fun saveImage(finalBitmap: Bitmap): String {
+
+        var imagePath = ""
+        val root = Environment.getExternalStorageDirectory().toString()
+        val myDir = File("$root/")
+        myDir.mkdirs()
+        val generator = Random()
+        var n = 10000
+        n = generator.nextInt(n)
+        val outletFrame = "Image-$n.jpg"
+        val file = File(myDir, outletFrame)
+        if (file.exists()) file.delete()
+        try {
+            val out = FileOutputStream(file)
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 20, out)
+            imagePath = file.absolutePath
+            out.flush()
+            out.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return imagePath
+    }
+
+    fun pickImageFromGallery(): Intent{
+        val intent = Intent (Intent.ACTION_PICK)
+        intent.type = "image/*"
+        return intent
     }
 
     class Factory(private val eventRepository: EventRepository) : ViewModelProvider.Factory {

33ab567fd1072d193e89daf24ef2de84ffdbba94
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-05-09 16:37:25 +0200

    merge
    Took 4 minutes

--- app/src/main/java/com/example/event_app/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/event_app/ui/activity/MainActivity.kt
@@ -5,6 +5,8 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.Toast

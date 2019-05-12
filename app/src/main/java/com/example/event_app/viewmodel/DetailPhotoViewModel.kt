2b417b3e0971bb622c436b5e455be0e1e60bcd16
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-05-12 18:22:36 +0200

    feat/ notification when photo reported and shimmer effect added to home page when refresh
    Took 6 hours 53 minutes

--- app/src/main/java/com/example/event_app/viewmodel/DetailPhotoViewModel.kt
+++ app/src/main/java/com/example/event_app/viewmodel/DetailPhotoViewModel.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.PixelFormat
 import android.os.Environment
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.event_app.model.Commentaire
@@ -12,8 +11,12 @@ import com.example.event_app.model.Event
 import com.example.event_app.model.Photo
 import com.example.event_app.model.User
 import com.example.event_app.repository.EventRepository
+import com.example.event_app.repository.NotificationRepository
 import com.example.event_app.repository.UserRepository
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.storage.StorageReference
 import io.reactivex.Completable
 import io.reactivex.Maybe
@@ -24,11 +27,11 @@ import timber.log.Timber
 import java.io.File
 import java.io.FileOutputStream
 
-class DetailPhotoViewModel(private val eventsRepository: EventRepository, private val userRepository: UserRepository) : BaseViewModel() {
+class DetailPhotoViewModel(private val eventsRepository: EventRepository, private val userRepository: UserRepository, private val notificationRepository: NotificationRepository) : BaseViewModel() {
+
     val photo: BehaviorSubject<Photo> = BehaviorSubject.create()
     val commentaires: BehaviorSubject<List<Commentaire>> = BehaviorSubject.create()
     val peopleWhoLike: BehaviorSubject<List<User>> = BehaviorSubject.create()
-
     private val folderName = "Event-Moi-Ca"
 
 
@@ -38,7 +41,7 @@ class DetailPhotoViewModel(private val eventsRepository: EventRepository, privat
                 getNumberOfLikes(photoId)
                 eventsRepository.getPhotoDetail(eventId, photoId).subscribe(
                     { picture ->
-                        Log.d("DetailEvent", "vm" + picture.url)
+                        Timber.d( "vm: ${picture.url}")
                         photo.onNext(picture)
                     },
                     { error ->
@@ -47,7 +50,7 @@ class DetailPhotoViewModel(private val eventsRepository: EventRepository, privat
 
                 eventsRepository.fetchCommentaires(photoId).subscribe(
                     {
-                        Log.d("DetailEvent", "getCommentaires ${it[0]}")
+                        Timber.d( "getCommentaires ${it[0]}")
                         commentaires.onNext(it)
                     },
                     {
@@ -126,17 +129,50 @@ class DetailPhotoViewModel(private val eventsRepository: EventRepository, privat
     }
     fun addLikes(photoId: String, user: User){
         eventsRepository.addLikes(photoId, user).subscribe({
-            Log.d("DetailPhoto", "addLikes")
+            Timber.d("addLikes")
         },{
             Timber.e(it)
         }).addTo(disposeBag)
         getNumberOfLikes(photoId)
     }
 
-    class Factory(private val eventsRepository: EventRepository, private val userRepository: UserRepository) : ViewModelProvider.Factory {
+    fun sendReportMessageToEventOwner(eventOwner: String) {
+            notificationRepository.sendMessageToSpecificChannel(eventOwner)
+        }
+
+
+    fun initMessageReceiving() {
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Timber.w(task.exception, "getInstanceId failed")
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = "message with token = $token"
+                Timber.d(msg)
+            })
+
+        FirebaseMessaging.getInstance().subscribeToTopic("notif_event_moi_ca").addOnCompleteListener {
+                task ->
+            var msg = "subscribed !!!"
+            if (!task.isSuccessful) {
+                msg = "failed to subscribed"
+            }
+            Timber.d("message for subscribing: $msg")
+        }
+
+    }
+
+    class Factory(private val eventsRepository: EventRepository, private val userRepository: UserRepository, private val notificationRepository: NotificationRepository) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             @Suppress("UNCHECKED_CAST")
-            return DetailPhotoViewModel(eventsRepository, userRepository) as T
+            return DetailPhotoViewModel(eventsRepository, userRepository, notificationRepository) as T
         }
     }
 }
